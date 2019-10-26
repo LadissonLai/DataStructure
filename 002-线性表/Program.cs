cad6115f3887d024b0ea58812bce137e46ea94dd
Ladisson-git <ladisson@example.com>
2019-10-26 14:26:00 +0800

    使用.NetCore创建一个项目

---
+++ 002-线性表/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _002_线性表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
