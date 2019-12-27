865d3ca2b9ed25b237fce0d8ab214de4f1f3b7e0
Ladisson-co <sealqq_653@outlook.com>
2019-12-27 15:37:12 +0800

    增加

---
+++ 100-MyTest/IMyInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100_MyTest
+{
+    interface IMyInterface
+    {
+        string Name { get; set; }
+        void PrintInfo();
+    }
+}
---
+++ 100-MyTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100_MyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowArray1();
+
+            ShowArray2();
+
+            ShowArray3();
+
+            ShowArray4();
+
+            MyType type = MyType.Dian;
+            Console.WriteLine(Convert.ToInt32(type));
+            Console.WriteLine(type);
+            Console.WriteLine(type.ToString());
+
+            Console.ReadKey();
+
+        }
+
+        static void ShowArray1()
+        {
+            //方式一
+            string[] arr1 = new string[] { "mm", "gg" };
+            foreach (var item in arr1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void ShowArray2()
+        {
+            string[] arr1 = new string[2] { "mm", "gg" };
+            foreach (var item in arr1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void ShowArray3()
+        {
+            string[] arr1 = { "mm", "gg" };
+            foreach (var item in arr1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void ShowArray4()
+        {
+            string[] arr1 = new string[5];
+            arr1 = new string[] { "bai", "du" };
+            foreach (var item in arr1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public enum MyType
+        {
+            Dian,
+            Ying,
+        }
+    }
+}
